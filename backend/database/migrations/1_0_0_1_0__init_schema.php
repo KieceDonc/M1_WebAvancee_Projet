278013255c886f5f8baa77b06eaa8e4f47844132
Whitlyne <hugo.bertoux@icloud.com>
2022-11-08 08:53:18 +0100

    ✨(backend) - Création des tables pour la bases de données

--- backend/database/migrations/1_0_0_1_0__init_schema.php
+++ backend/database/migrations/1_0_0_1_0__init_schema.php
@@ -10,13 +10,30 @@ return new class extends Migration {
    * @return void
    */
   public function up() {
-    Schema::create('flights', function (Blueprint $table) {
-      $table->id();
-      $table->string('name');
-      $table->string('airline');
-      $table->timestamps();
+    Schema::create('Users', function (Blueprint $table) {
+      $table->primary('id');
+      $table->string('first_name');
+      $table->string('last_name');
+      $table->string('email');
+      $table->date('account_creation');
+      $table->date("account_modified");
   });
-    
+
+    Schema::create('Car',function(Blueprint $table) {
+      $table->primary('id');
+      $table->string('name');
+      $table->float("price");
+      $table->enum('Sportive','SUV','Coupe');
+      $table->integer('nb_doors');
+      $table->integer('year');
+      $table->string('description');
+    }); 
+
+    Schema::create('Panier',function(Blueprint $table){
+      $table->primary('id');
+      $table->integer('idUtilisateur')->references("id")->on("Users");
+      $table->json("data");
+    })
   }
 
   /**
@@ -25,6 +42,8 @@ return new class extends Migration {
    * @return void
    */
   public function down() {
-   
+    Schema::drop("Users");
+    Schema::drop('Car');
+    Schema::drop("Panier");
   }
 };
