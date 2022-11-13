174e573503a5b8e3330fa481dfe32543fe658d9f
Cybermade <hamidounazim@gmail.com>
2022-11-13 18:51:59 +0100

    added routes for login and register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,23 @@ import './App.css'
 import Register from './Register'
 import Login from './Login'
 import Profile from './Profile'
+import HomePage from './HomePage'
+import Button from "@mui/material/Button";
+import { Link,BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+
 
 function App() {
 
   return (
+    <Router>
     <div>
-        <Register/>
-        <Login/>
-        <Profile/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
     </div>
+    </Router>
   )
 }
 
