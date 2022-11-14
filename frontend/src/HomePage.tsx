55324935c31f2983c91c51dc18568665143a186c
Cybermade <hamidounazim@gmail.com>
2022-11-14 11:10:00 +0100

    💄(frontend) : ajout de fichier css

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 import React,{useState} from "react";
 import { Link,BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import { Button } from "@mui/material";
+import "./HomePage.css";
 
 function HomePage(){
     
     return(
-        <div>
+        <div className="homepage">
             <h1>Home Page TODO</h1>
             <Button component={Link} to="/login" variant="contained">Login</Button>
             <Button component={Link} to="/register" variant="contained">Register</Button>
