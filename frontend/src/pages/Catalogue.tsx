e507e2151d27a96906fd20dd66deebdc644216ef
KieceDonc <valentin.verstracte@gmail.com>
2022-12-29 23:42:58 +0100

    :sparkles: (front) - Catalogue CSS

--- frontend/src/pages/Catalogue.tsx
+++ frontend/src/pages/Catalogue.tsx
@@ -21,18 +21,16 @@ import { Car } from '../models/interface'
 
 function Catalogue() {
   const data = useAppSelector((state) => state.cardata.cardata)
-  const pictures = useAppSelector((state)=>state.cardata.picturesdata)
+  const pictures = useAppSelector((state) => state.cardata.picturesdata)
 
   // Etat des tries actifs
   const [TrieAlpha, setTrieAlpha] = React.useState<string>('1')
   const [tabData, settabData] = React.useState<Car[]>([])
 
-
   let dataTab: Car[] = Object.values(data.dataCar)
 
-  const [tabPrix ,settabPrix] = React.useState<Car[]>(dataTab)
-  const [tabType,settabType] = React.useState<Car[]>(dataTab)
-
+  const [tabPrix, settabPrix] = React.useState<Car[]>(dataTab)
+  const [tabType, settabType] = React.useState<Car[]>(dataTab)
 
   // Simular as ComponentMount in VueJS
   React.useEffect(() => {
@@ -49,10 +47,10 @@ function Catalogue() {
 
   React.useEffect(() => {
     Trieraveclestri()
-  }, [tabPrix,tabType])
+  }, [tabPrix, tabType])
 
   // Retrieve Type of all car
-  const GetAllTypes = (tabData: Car[]):string[] => {
+  const GetAllTypes = (tabData: Car[]): string[] => {
     let uniqueValues: string[] = []
     tabData.forEach((obj: Car) => {
       if (uniqueValues.indexOf(obj.type) === -1) {
@@ -65,9 +63,9 @@ function Catalogue() {
 
   // Fonction de tri
 
-  const TriAlphabetique = (enter: boolean,tab:Car[]):void => {
+  const TriAlphabetique = (enter: boolean, tab: Car[]): void => {
     settabData(
-      tab.sort(function (a:Car, b:Car) {
+      tab.sort(function (a: Car, b: Car) {
         let aname: string = a.name
         let bname: string = b.name
         return aname.localeCompare(bname)
@@ -78,17 +76,17 @@ function Catalogue() {
     }
   }
 
-  function Trieraveclestri():void {
-    let i : Car[]= tabPrix.filter(element => tabType.includes(element));
-    TrieAlpha == '1' ? TriAlphabetique(true,i) : TriAlphabetique(false,i)
+  function Trieraveclestri(): void {
+    let i: Car[] = tabPrix.filter((element) => tabType.includes(element))
+    TrieAlpha == '1' ? TriAlphabetique(true, i) : TriAlphabetique(false, i)
   }
 
   // Function for handle change on filter item
 
-  const handleChangeAlpha = (event: SelectChangeEvent):void => {
+  const handleChangeAlpha = (event: SelectChangeEvent): void => {
     let targetValue: string = event.target.value
     setTrieAlpha(targetValue)
-    targetValue == '1' ? TriAlphabetique(true,tabData) : TriAlphabetique(false,tabData)
+    targetValue == '1' ? TriAlphabetique(true, tabData) : TriAlphabetique(false, tabData)
   }
 
   return (
@@ -96,32 +94,40 @@ function Catalogue() {
       <Typography variant="h2" textAlign={'center'}>
         Catalogue
       </Typography>
-      <FormControl sx={{ width: '45%', m: '10px' }}>
-        <InputLabel id="demo-simple-select-label">Trier par Ordre Alphabétique</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={TrieAlpha}
-          label="Trier par ordre alphabétique"
-          onChange={handleChangeAlpha}>
-          <MenuItem value={1}>A-Z</MenuItem>
-          <MenuItem value={2}>Z-A</MenuItem>
-        </Select>
-      </FormControl>
-      <TriType type={type} tabdata={dataTab} onSort={(tabDataTrie:Car[]) => settabType(tabDataTrie)} />
-      <TriPrix tabdata={dataTab} onSort={(tabDataTrie:Car[]) => settabPrix(tabDataTrie)} />
+      <div className="catalgue-criterias-container">
+        <FormControl sx={{ width: '20vw', m: '10px' }}>
+          <InputLabel id="demo-simple-select-label">Trier par Ordre Alphabétique</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={TrieAlpha}
+            label="Trier par ordre alphabétique"
+            onChange={handleChangeAlpha}>
+            <MenuItem value={1}>A-Z</MenuItem>
+            <MenuItem value={2}>Z-A</MenuItem>
+          </Select>
+        </FormControl>
+        <TriType
+          type={type}
+          tabdata={dataTab}
+          onSort={(tabDataTrie: Car[]) => settabType(tabDataTrie)}
+        />
+      </div>
+      <div className="catalogue-pricesort-container">
+        <TriPrix tabdata={dataTab} onSort={(tabDataTrie: Car[]) => settabPrix(tabDataTrie)} />
+      </div>
       <Typography variant="h5" textAlign="center" marginBottom={'20px'}>
         Liste des voitures
       </Typography>
-      {tabData.map((item: Car,index:number) => (
+      {tabData.map((item: Car, index: number) => (
         <Card
           key={index}
           sx={{
-            display: 'flex',
-            width: '65%',
-            border: '1px dashed grey',
+            display: 'block',
+            width: '50%',
+            border: '1px grey',
             m: 'auto',
-            mb: '20px',
+            mb: '40px',
             alignContent: 'center',
             textAlign: 'center',
           }}>
@@ -129,21 +135,23 @@ function Catalogue() {
             <CardMedia
               component="img"
               alt="Contemplative Reptile"
-              height="140"
-              image={pictures[item.id].srcPicturesCar?"/"+pictures[item.id].srcPicturesCar:""}
+              image={pictures[item.id].srcPicturesCar ? '/' + pictures[item.id].srcPicturesCar : ''}
               title="Contemplative Reptile"
             />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
+              <Typography gutterBottom variant="h4" component="h2">
                 {item.name}
               </Typography>
-              <Typography variant="body2" component="p">
-                Prix : {item.price} €
-              </Typography>
+              <Typography component="p">Prix : {item.price} €</Typography>
             </CardContent>
           </CardActionArea>
-          <CardActions>
-            <Button component={Link} to={'/carPage/' + item.id} variant="contained" size="small">
+          <CardActions sx={{ padding: 0 }}>
+            <Button
+              component={Link}
+              to={'/carPage/' + item.id}
+              variant="contained"
+              size="medium"
+              sx={{ width: '100%', borderRadius: 0 }}>
               Aller sur la page
             </Button>
           </CardActions>
