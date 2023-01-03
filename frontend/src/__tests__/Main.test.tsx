35163f38d03908c6aa02c07f313ce757e08b709a
RenardBOT <pevan.ers@gmail.com>
2023-01-03 18:59:32 +0100

    :white_check_mark: Some tests to verify coverage works

--- frontend/src/__tests__/Main.test.tsx
+++ frontend/src/__tests__/Main.test.tsx
@@ -1,7 +1,57 @@
-import react from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../App'
+import './index.css'
+import { store } from '../app/store'
+import { Provider } from 'react-redux'
+import CharteUtilisation from '../pages/CharteUtilisation'
+import { render } from '@testing-library/react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import TriPrix from '../components/TriPrix'
+import TriType from '../components/TriType'
+import Contact from '../pages/Contact'
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 describe('Environnement de test', () => {
-    test('Devrait passer', () => {
-        expect(true).toBe(true);
-    });
+    it("should render Charte", () => {
+        render(
+            <Router>
+                <CharteUtilisation />
+            </Router>
+        )
+    })
+
+    it("should render Contact", () => {
+        render(
+            <Router>
+                <Contact />
+            </Router>
+        )
+    })
+
+    it("should render Footer", () => {
+        render(
+            <Router>
+                <Footer />
+            </Router>
+        )
+    })
+
+    it("should render Header", () => {
+        render(
+            <Router>
+                <Header />
+            </Router>
+        )
+    })
+
+    it("should render TriPrix", () => {
+        render(<TriPrix />)
+    })
+
+    it("should render TriType", () => {
+        render(<TriType />)
+    })
 });
