e87530949532bb21dbc68b2f74b4bcfe5f34f0de
RenardBOT <pevan.ers@gmail.com>
2023-01-03 18:59:44 +0100

    :white_check_mark: Testing app component

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,16 +1,22 @@
 
 import { Provider } from 'react-redux';
-import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { MemoryRouter, Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import { getByTestId, render } from '@testing-library/react';
 import React from 'react';
 import App from '../App'
-import { store } from '../app/store'
 import './index.css'
+import { configureStore } from '@reduxjs/toolkit';
+import { car1, car2, user1 } from '../__mocks__/objectsMock'
+import mockCardata from '../__mocks__/cardataMock'
+import Catalogue from '../pages/Catalogue';
+import { store } from '../__mocks__/storeMock'
 
-
-
-test('App s\'affiche correctement', () => {
-
-  render(<App />);
-
+describe('App : Integration', () => {
+  it("should render App", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
 });
